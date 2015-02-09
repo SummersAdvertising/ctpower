04aa1f624667efb8cce44e32ef82f3fe39b25b69
Adam <adam@summers.com.tw>
2015-02-09 11:08:21 +0800

    isolate recaptcha key

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,17 +1,20 @@
 Recaptcha.configure do |config|
 
-  if Rails.env.production? 
+  if Rails.env.production?
+    #citypower.com.tw
+    config.public_key  = '6LcutwETAAAAAApO76YrrXBZOLj_wvFSZo8P88ci'
+    config.private_key = '6LcutwETAAAAAC8OQiMSXt5fFdaAmOlo32Vgdj1f'
     #Temporary: li408-68
-    config.public_key  = '6LdSLv8SAAAAADxqajH-n9OAujouUpPlqV7CRPii'
-    config.private_key = '6LdSLv8SAAAAAGJEDHOBPRGkeP9W4ZPf7LZOQKFi'
+    #config.public_key  = '6LdSLv8SAAAAADxqajH-n9OAujouUpPlqV7CRPii'
+    #config.private_key = '6LdSLv8SAAAAAGJEDHOBPRGkeP9W4ZPf7LZOQKFi'
     #Temporary: ctpower.summers.com.tw
     #config.public_key  = '6LcxNP0SAAAAAKeQ9dJMP0M-lVJavUPrZ3EsxBfG'
     #config.private_key = '6LcxNP0SAAAAAG0S4WVIgxZn3IN5r2TntJOUR28m'
 
-  else 
-    #Temporary: www.citypower.com.tw, because localhost is ok
-    config.public_key  = '6LcBNf0SAAAAAEfitPESm4zYo3Xml6FhpIERfm02'
-    config.private_key = '6LcBNf0SAAAAAFsz6lrQ8hgFGIvXnyarJSPM-mSn'
-    
+  else
+    #Temporary: citypower.com.tw
+    config.public_key  = '6LcutwETAAAAAApO76YrrXBZOLj_wvFSZo8P88ci'
+    config.private_key = '6LcutwETAAAAAC8OQiMSXt5fFdaAmOlo32Vgdj1f'
+
   end
 end
